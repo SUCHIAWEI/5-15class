2e59effc5573f37612ee42a7fb15754b23200e00
SUCHIAWEI <32055784+SUCHIAWEI@users.noreply.github.com>
2018-05-21 23:04:24 +0800

    hw

--- src/FrameC.java
+++ src/FrameC.java
@@ -3,244 +3,220 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-    public class FrameC extends JFrame{
-        private JButton n1 = new JButton("1");
-        private JButton n2 = new JButton("2");
-        private JButton n3 = new JButton("3");
-        private JButton n4 = new JButton("4");
-        private JButton n5 = new JButton("5");
-        private JButton n6 = new JButton("6");
-        private JButton n7 = new JButton("7");
-        private JButton n8 = new JButton("8");
-        private JButton n9 = new JButton("9");
-        private JButton n0 = new JButton("0");
-        private JButton s1 = new JButton("/");
-        private JButton s2 = new JButton("*");
-        private JButton s3 = new JButton("-");
-        private JButton s4 = new JButton(".");
-        private JButton s5 = new JButton("=");
-        private JButton s6 = new JButton("+");
-        private JLabel lb = new JLabel(" ");
-        private Container cp ;
-        private JPanel jp1 = new JPanel(new GridLayout(4,4,3,3));
-
-        public FrameC(){
-            init();
-        }
-        protected void init(){
-            this.setBounds(100,100,425,500);
-            this.setVisible(true);
-            cp=this.getContentPane();
-            cp.setLayout(new BorderLayout(4,4));
-            cp.add(jp1,BorderLayout.CENTER);
-            cp.add(lb,BorderLayout.NORTH);
-            lb.setFont(new Font(null,Font.BOLD,40));
-
-            jp1.add(n7); jp1.add(n8); jp1.add(n9); jp1.add(s1);
-            jp1.add(n4); jp1.add(n5); jp1.add(n6); jp1.add(s2);
-            jp1.add(n1); jp1.add(n2); jp1.add(n3); jp1.add(s3);
-            jp1.add(n0); jp1.add(s4); jp1.add(s5); jp1.add(s6);
-
-            n1.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("1");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "1");
-                    }
-                }
-            });
-
-            n2.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("2");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "2");
-                    }
-                }
-            });
-
-            n3.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("3");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "3");
-                    }
-                }
-            });
-
-            n4.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("4");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "4");
-                    }
-                }
-            });
-
-            n5.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("5");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "5");
-                    }
-                }
-            });
-
-            n6.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("6");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "6");
-                    }
-                }
-            });
-
-            n7.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("7");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "7");
-                    }
-                }
-            });
-
-            n8.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("8");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "8");
-                    }
-                }
-            });
-
-            n9.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("9");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "9");
-                    }
-                }
-            });
-
-            n0.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("0");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "0");
-                    }
-                }
-            });
-
-            s1.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("/");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "/");
-                    }
-                }
-            });
-
-            s2.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                    if (lb.getText().equals(" ")){
-                        lb.setText("*");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "*");
-                    }
-                }
-            });
-
-            s3.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("-");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "-");
-                    }
-                }
-            });
-
-            s4.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText(".");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + ".");
-                    }
-                }
-            });
-
-            s5.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("=");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "=");
-                    }
-                }
-            });
-
-            s6.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if (lb.getText().equals(" ")){
-                        lb.setText("+");
-                    }
-                    else if (!lb.getText().equals(" ")) {
-                        lb.setText(lb.getText() + "+");
-                    }
-
-                }
-            });
-
-
-
-
-
-
-
-        }
+public class FrameC extends JFrame {
+    private JButton jb[] = new JButton[20];
+    private JLabel lb = new JLabel("0");
+    private Container cp;
+    private JPanel jp1 = new JPanel(new GridLayout(4,5,3,3));
+    int op =-1;
+    boolean flag =true;
+    Double v1=0.0;
+    Double v2=0.0;
+    public  FrameC(){
+        init();
     }
 
+    private void init(){
+        this.setBounds(100,100,425,525);
+
+        cp=this.getContentPane();
+        cp.add(jp1,BorderLayout.CENTER);
+        cp.add(lb,BorderLayout.NORTH);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        lb.setFont(new Font(null,Font.BOLD,40));
+
+        for (int i=0 ;i<20 ;i++){
+
+            switch (i){
+                case 0:
+                    jb[i]=new JButton("7");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 1:
+                    jb[i]=new JButton("8");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 2:
+                    jb[i]=new JButton("9");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 3:
+                    jb[i]=new JButton("/");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 4:
+                    jb[i]=new JButton("Ac");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 5:
+                    jb[i]=new JButton("4");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 6:
+                    jb[i]=new JButton("5");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 7:
+                    jb[i]=new JButton("6");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 8:
+                    jb[i]=new JButton("*");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 9:
+                    jb[i]=new JButton("sqrt");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 10:
+                    jb[i]=new JButton("1");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 11:
+                    jb[i]=new JButton("2");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 12:
+                    jb[i]=new JButton("3");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 13:
+                    jb[i]=new JButton("-");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 14:
+                    jb[i]=new JButton("PI");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 15:
+                    jb[i]=new JButton("0");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 16:
+                    jb[i]=new JButton(".");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 17:
+                    jb[i]=new JButton("=");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 18:
+                    jb[i]=new JButton("+");
+                    jp1.add(jb[i]);
+                    break;
+
+                case 19:
+                    jb[i]=new JButton("Exit");
+                    jp1.add(jb[i]);
+                    break;
+            }
+            jb[i].setFont(new Font(null,Font.CENTER_BASELINE,20));
+            jb[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton tmp = (JButton) e.getSource();
+                    switch (tmp.getText()) {
+                        case "Ac":
+                            lb.setText("0");
+                            op = -1;
+                            break;
+
+                        case ".":
+                            if (Double.parseDouble(lb.getText())%2==0 || Double.parseDouble(lb.getText())%2==1){
+                                lb.setText(lb.getText()+".");
+                            }
+                            else { lb.setText(lb.getText());
+                            }
+
+                            break;
+
+                        case "PI":
+                            lb.setText(Double.toString(Math.PI));
+                            break;
+                        case "+":
+                            op = 0;
+                            v1 = Double.parseDouble(lb.getText());
+                            flag = false;
+                            break;
+                        case "-":
+                            op = 1;
+                            v1 = Double.parseDouble(lb.getText());
+                            flag = false;
+                            break;
+                        case "*":
+                            op = 2;
+                            v1 = Double.parseDouble(lb.getText());
+                            flag = false;
+                            break;
+                        case "/":
+                            op = 3;
+                            v1 = Double.parseDouble(lb.getText());
+                            flag = false;
+                            break;
+
+                        case "sqrt":
+                            v1 = Double.parseDouble(lb.getText());
+                            lb.setText(Double.toString(Math.sqrt(v1)));
+                            break;
+                        case "Exit":
+                            System.exit(0);
+                            break;
+
+                        case "=":
+                            v2 = Double.parseDouble(lb.getText());
+                            Double v3 = 0.0;
+                            flag = true;
+
+                            switch (op) {
+                                case 0:
+                                    v3 = v1 + v2;
+                                    break;
+                                case 1:
+                                    v3 = v1 - v2;
+                                    break;
+                                case 2:
+                                    v3 = v1 * v2;
+                                    break;
+                                case 3:
+                                    v3 = v1 / v2;
+                                    break;
+                            }
+                            lb.setText(Double.toString(v3));
+                            break;
+
+                        default:
+                            if (lb.getText().equals("0")|| (op > -1 && flag == false) ) {
+                                lb.setText(tmp.getText());
+                                if (flag == false){
+                                    flag = true;
+                                }
+                            }
+                            else {
+                                lb.setText(lb.getText() + tmp.getText());
+                            }
+                            break;
+                    }
+                }
+            });
+        }
+    }
+}
